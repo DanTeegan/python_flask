5dedeea39b2fc1c1e8863478ed07d4dd95e369b0
dan. teegan <dan.teegan@hotmail.com>
2020-07-21 11:00:03 +0100

    Python flask

--- app.py
+++ app.py
@@ -8,11 +8,21 @@ app = Flask(__name__) # This is the standard syntax to create a flask instance
 # Block of code for default page
 @app.route("/")
 
-
 # Create a method to display on the home page
 def index():
     return "<h1>Welcome to MVC with flask project </h1>"
 # This is the end of the block of code for the default page
+
+# Exercise - create a function called welcome_user
+# Create a decorator to link the page / user
+# Return "Welcome to Python flask app dear" + {name}
+# We want to achieve - In the browser when we load the page from the homepage to username i.e your name
+# It should display your name in the browser with message from your method
+@app.route("/Daniel")
+
+def welcome_user():
+    return "Welcome to Python flask app dear Daniel"
+
 # Index method will be called at the end point (where we connect to)
 # The Index method will display on our home/default page
 
@@ -21,6 +31,8 @@ def index():
 # Syntax to run our app
 if __name__ == "__main__":
     app.run(debug=True) # Good practice to add debug = true
+
+
 
 
 
